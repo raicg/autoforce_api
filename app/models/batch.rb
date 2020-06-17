ac995c9e14d6e623ab77525a157e58b5a2c6b1f2
Raí Gondim <raicg2@gmail.com>
2020-06-17 18:02:02 -0300

    creating a before_validation on Batch model to attach automatically the orders to a batch

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,10 +1,16 @@
 class Batch < ApplicationRecord
   has_many :orders
 
+  before_validation :attach_orders
+
   validates :orders, presence: true
   validate :orders_purchase_channel_must_be_the_same
 
   private
+
+  def attach_orders
+    orders << Order.where(status: 'ready', purchase_channel: purchase_channel, batch_id: nil)
+  end
 
   def orders_purchase_channel_must_be_the_same
     errors.add("Every order on the batch must have the same purchase channel") unless orders.map { |o| o.purchase_channel }.uniq.length == 1
