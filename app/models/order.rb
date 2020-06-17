fab909d248e73a882db525c5a662cf2808876ac5
Raí Gondim <raicg2@gmail.com>
2020-06-17 17:02:45 -0300

    including AASM on Order model and fixing bugs

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,23 +1,25 @@
 class Order < ApplicationRecord
+  include AASM
+
   belongs_to :batch, optional: true
 
   enum status: [:ready, :production, :closing, :sent]
 
-  aasm column: :state, enum: true do
+  aasm column: :status, enum: true do
     state :ready, initial: true
     state :production
     state :closing
     state :sent
 
-    event :produce do
+    event :produce_order do
       transitions from: :ready, to: :production
     end
 
-    event :close do
+    event :close_order do
       transitions from: :production, to: :closing
     end
 
-    event :send do
+    event :send_order do
       transitions from: :closing, to: :sent
     end
   end
