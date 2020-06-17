b381fd717622ec646a9913a39c9ee246d2e6f75b
Raí Gondim <raicg2@gmail.com>
2020-06-17 18:01:14 -0300

    creating validation that all orders must have the same purchase channel on Batch model

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,4 +2,11 @@ class Batch < ApplicationRecord
   has_many :orders
 
   validates :orders, presence: true
+  validate :orders_purchase_channel_must_be_the_same
+
+  private
+
+  def orders_purchase_channel_must_be_the_same
+    errors.add("Every order on the batch must have the same purchase channel") unless orders.map { |o| o.purchase_channel }.uniq.length == 1
+  end
 end
