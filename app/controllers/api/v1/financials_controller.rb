eb63bd54c68f45fe73c03a14cb67d48a0b374570
Raí Gondim <raicg2@gmail.com>
2020-06-18 02:41:30 -0300

    creating Api V1 Financials controller and the index action

---
+++ app/controllers/api/v1/financials_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::FinancialsController < ApplicationController
+  def index
+    # Order.all.group_by(&:purchase_channel)
+    json_answer = {}
+    orders = []
+    Order.find_in_batches.each do |batch|
+      batch.each do |order|
+        orders << order
+      end
+    end
+    orders = orders.group_by(&:purchase_channel)
+    orders.each do |orders_grouped|
+      json_answer.merge!("purchase_channel #{orders_grouped[0]}":
+                            ['orders_count': orders_grouped[1].count,
+                             'total_value': orders_grouped[1].map { |o| o.total_value }.sum])
+    end
+
+    render json: json_answer, status: :ok
+  end
+end
