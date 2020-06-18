1b7d2be04b3710711f2a90bfe30044dc1ab9d39a
Raí Gondim <raicg2@gmail.com>
2020-06-18 16:48:27 -0300

    configuring rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,15 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'sidekiq/testing'
+
+ActiveJob::Base.queue_adapter = :test
+Sidekiq::Testing.fake!
+
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
