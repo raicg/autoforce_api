8e2864192657255b01b602bbfb8992d9dc3218da
Raí Gondim <raicg2@gmail.com>
2020-06-18 02:40:41 -0300

    finalizing custom methods of Api V1 Batches controller (close_orders and send_orders)

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::BatchesController < ApiController
   def close_orders
+    render json: { status: t(:all_orders_closed) },
+           status: :ok if Batch.find(permited_params[:id]).close_orders
   end
 
   def send_orders
+    render json: { status: t(:all_orders_sent) },
+           status: :ok if Batch.find(permited_params[:id]).send_orders(permited_params[:delivery_service])
+  end
+
+  private
+
+  def permited_params
+    params.permit(:id, :delivery_service)
   end
 end
