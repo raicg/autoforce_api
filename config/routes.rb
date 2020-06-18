cf89f1ffb11fd80a9df53f051ff02431e5a85893
Raí Gondim <raicg2@gmail.com>
2020-06-17 23:20:01 -0300

    creating all api endpoints routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      jsonapi_resources :orders, only: [:index, :show, :create]
+      jsonapi_resources :batches, only: [:index, :show, :create] do
+        member do
+          post :close_orders
+          post :send_orders
+        end
+      end
+    end
+  end
 end
