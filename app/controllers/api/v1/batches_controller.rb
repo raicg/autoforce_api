21615641f5269011f5a42d40da361c91497ee3d4
Raí Gondim <raicg2@gmail.com>
2020-06-19 09:56:11 -0300

    fixing api v1 batches controller to handle with the errors (batch not found or bad request)

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -1,12 +1,24 @@
 class Api::V1::BatchesController < ApiController
   def close_orders
-    render json: { status: t(:all_orders_closed) },
-           status: :ok if Batch.find(permited_params[:id]).close_orders
+    if batch = Batch.find_by(id: permited_params[:id])
+      batch.close_orders
+      render json: { message: t(:all_orders_closed), status: :ok }
+    else
+      render json: { message: t(:batch_not_found, scope: 'errors.messages'), status: :not_found } 
+    end
   end
 
   def send_orders
-    render json: { status: t(:all_orders_sent) },
-           status: :ok if Batch.find(permited_params[:id]).send_orders(permited_params[:delivery_service])
+    unless permited_params[:delivery_service].nil?
+      if batch = Batch.find_by(id: permited_params[:id])
+        batch.send_orders(permited_params[:delivery_service])
+        render json: { message: t(:all_orders_sent), status: :ok }
+      else
+        render json: { message: t(:batch_not_found, scope: 'errors.messages'), status: :not_found }
+      end
+    else
+      render json: { message: t(:must_send_delivery_service_variable_as_parameter, scope: 'errors.messages'), status: :bad_request }
+    end
   end
 
   private
